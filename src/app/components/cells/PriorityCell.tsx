549e9cc20b66f893a724e6794a05545e6cddf661
Lucas Hocker <lucas.hocker@dynatrace.com>
2023-04-23 21:50:53 +0200

    bug: fix priority regression

--- src/app/components/cells/PriorityCell.tsx
+++ src/app/components/cells/PriorityCell.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { CloudHostStatus, CloudType } from '../../types/CloudTypes';
+import { CloudType } from '../../types/CloudTypes';
 import { useHostsStatus } from '../../hooks/useHostsStatus';
 import { ErrorIcon } from '@dynatrace/strato-icons';
-import { format, units } from '@dynatrace-sdk/util-formatters';
 import { Indicator } from '../Indicator';
 import { useOneAgentHosts } from 'src/app/hooks/useOneAgentHosts';
 import { coverageRatio } from './coverage-ratio';
-
-
 
 type PriorityCellProps = {
   type: CloudType;
@@ -27,12 +24,17 @@ export const PriorityCell = ({ type }: PriorityCellProps) => {
   const oneagentHosts = oneagent.data[type];
 
   const coverage = coverageRatio(status.data, oneagentHosts);
-  if (status.data.status && oneagentHosts !== null && oneagentHosts > 0)
-    return <Indicator state="critical">Critical</Indicator>;
-  if (coverage > 100) return <Indicator state="critical">Critical</Indicator>;
-  if (coverage == 100) return <>-</>;
+  //Critical: we don't know how big the problem even is, because we know there are more clouds than connected
+  if (!status.data.status && oneagentHosts !== null && oneagentHosts > 0)
+    return <Indicator state='critical'>Critical</Indicator>;
+  if (coverage > 100) return <Indicator state='critical'>Critical</Indicator>;
+  //Success: we have reached the target of 100%
+  if (coverage == 100) return <Indicator state='success'>-</Indicator>;
+  //Low/Medium: not perfect, but user making progress
   if (coverage >= 90) return <>Low</>;
   if (coverage > 70) return <>Medium</>;
-  if (coverage >= 0) return <Indicator state="warning">High</Indicator>;
+  //High: user needs to install many oneAgents
+  if (coverage >= 0) return <Indicator state='warning'>High</Indicator>;
+  //Unknown state: this should not be hit
   return <>-</>;
 };
