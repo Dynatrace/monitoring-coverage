10f3a88bc48bf56ab29686cf3eebf1198a753564
Lucas Hocker <lucas.hocker@dynatrace.com>
2023-09-14 21:53:41 +0200

    Fix-up - Fix typo in app intro

--- app.config.ts
+++ app.config.ts
@@ -6,7 +6,7 @@ const config: CliOptions = {
   icon: './src/assets/logo.png',
   app: {
     name: 'Monitoring Coverage',
-    version: '0.0.17',
+    version: '0.0.18',
     description: 'A sample app helping you get to 100% cloud coverage',
     id: 'my.monitoring.coverage',
     scopes: [
