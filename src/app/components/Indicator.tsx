49e15d9e31916b8f380242458e84a0c4fb919a3f
Lucas Hocker <lucas.hocker@dynatrace.com>
2023-09-14 23:09:20 +0200

    clean unused deps

--- src/app/components/Indicator.tsx
+++ src/app/components/Indicator.tsx
@@ -1,11 +1,8 @@
 import React, { ReactNode } from 'react';
 import { Colors } from '@dynatrace/strato-design-tokens';
-import { Flex, Text } from '@dynatrace/strato-components-preview';
 
 type IndicatorState = 'critical' | 'success' | 'warning' | 'neutral';
-
 type IndicatorStateMap = { [key in IndicatorState]: string };
-
 type IndicatorProps = {
   state: IndicatorState;
   children: ReactNode;
